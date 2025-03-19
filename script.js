538c9e501dc9d072151ab2a8bde693ed05259ab6
school-stuff13 <c7440134@gmail.com>
2025-03-19 15:32:30 +0000

    Particles

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function loadNewPage () {
 function loadOldPage () {
     window.location.href = "index.html"
 }
+
+const canvas = document.getElementById('particalCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Particle {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.size > 0.2) this.size -= 0.1;
+    }
+
+    draw() {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        ctx.beginPath();
+    }
+}
